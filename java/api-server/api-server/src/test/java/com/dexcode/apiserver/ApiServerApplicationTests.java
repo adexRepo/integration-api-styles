365b0ad310291a18e29f967e84defb0fcfae7f40
Adek Kristiyanto <adek.kristiyanto@sinarmas.com>
2023-10-28 15:27:56 +0700

    initial commit

---
+++ java/api-server/api-server/src/test/java/com/dexcode/apiserver/ApiServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dexcode.apiserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
